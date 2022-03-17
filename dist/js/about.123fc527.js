32700c37ef9357aeeb81735ec292c037a316ffcf
MonKokoro <735335659@qq.com>
2022-03-18 01:37:06 +0800

    up

---
+++ dist/js/about.123fc527.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkels_tools"]=self["webpackChunkels_tools"]||[]).push([[443],{4335:function(n,s,t){t.r(s),t.d(s,{default:function(){return f}});var e=t(3396);const u={class:"about"},a=(0,e._)("h1",null,"This is an about page",-1),o=[a];function r(n,s){return(0,e.wg)(),(0,e.iD)("div",u,o)}var l=t(89);const c={},i=(0,l.Z)(c,[["render",r]]);var f=i}}]);
+//# sourceMappingURL=about.123fc527.js.map
